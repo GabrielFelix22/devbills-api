97a0ba3ddbe21acdcc6f4adb7e140e85a658f073
Gabriel Felix <gabriel.alves2208@gmail.com>
2025-02-11 20:13:14 -0300

    commit: Criando as Factories, para controlar as requisições, não terem instancias infinitas.

--- src/controllers/categories.cotroller.ts
+++ src/controllers/categories.cotroller.ts
@@ -1,41 +1,35 @@
 import type { NextFunction, Request, Response } from 'express';
-import { z } from 'zod';
 
 import { StatusCodes } from 'http-status-codes';
-import { CategoriesRepository } from '../database/repositories/categories.repository';
-import { CategoryModel } from '../database/schemas/category.schema';
 import type { CreateCategoryDTO } from '../dtos/categories.dto';
-import { CategoriesService } from '../services/categories.service';
+import type { CategoriesService } from '../services/categories.service';
+
 export class CategoriesController {
-	async create(
-		req: Request<unknown, unknown, CreateCategoryDTO>,
-		res: Response,
-		next: NextFunction,
-	) {
-		try {
-			const { title, color } = req.body;
+  constructor(private categoriesService: CategoriesService) {}
 
-			const repository = new CategoriesRepository(CategoryModel);
-			const service = new CategoriesService(repository);
+  create = async (
+    req: Request<unknown, unknown, CreateCategoryDTO>,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      const { title, color } = req.body;
 
-			const result = await service.create({ title, color });
+      const result = await this.categoriesService.create({ title, color });
 
-			return res.status(StatusCodes.CREATED).json(result);
-		} catch (err) {
-			next(err);
-		}
-	}
+      return res.status(StatusCodes.CREATED).json(result);
+    } catch (err) {
+      next(err);
+    }
+  };
 
-	async index(req: Request, res: Response, next: NextFunction) {
-		try {
-			const repository = new CategoriesRepository(CategoryModel);
-			const service = new CategoriesService(repository);
+  index = async (_: Request, res: Response, next: NextFunction) => {
+    try {
+      const result = await this.categoriesService.index();
 
-			const result = await service.index();
-
-			return res.status(StatusCodes.OK).json(result);
-		} catch (err) {
-			next(err);
-		}
-	}
+      return res.status(StatusCodes.OK).json(result);
+    } catch (err) {
+      next(err);
+    }
+  };
 }
--- src/routes/categories.route.ts
+++ src/routes/categories.route.ts
@@ -2,19 +2,22 @@ import { Router } from 'express';
 
 import { CategoriesController } from '../controllers/categories.cotroller';
 import { createCategorySchema } from '../dtos/categories.dto';
+import { CategoriesFactory } from '../factories/categories.factory';
 import { ParamsType, validator } from '../middleware/validator.middleware';
 
 export const categoriesRoutes = Router();
 
-const controller = new CategoriesController();
+const controller = new CategoriesController(
+  CategoriesFactory.getServiceInstance(),
+);
 
 categoriesRoutes.get('/', controller.index);
 
 categoriesRoutes.post(
-	'/',
-	validator({
-		schema: createCategorySchema,
-		type: ParamsType.BODY,
-	}),
-	controller.create,
+  '/',
+  validator({
+    schema: createCategorySchema,
+    type: ParamsType.BODY,
+  }),
+  controller.create,
 );
