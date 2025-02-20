ad1e3c071dba2d17af0e3ca62c12942ce2b8b11f
Gabriel Felix <gabriel.alves2208@gmail.com>
2025-02-20 18:00:21 -0300

    commit: Configurando o CORS, Build e Ajustes Finais.

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,22 @@
 import express, { json } from 'express';
 import { routes } from './routes';
 import 'dotenv/config';
+
+import cors from 'cors';
 import { setupMongo } from './database';
 import { errorHandler } from './middleware/error-handler.middeware';
 
 setupMongo().then(() => {
-	const app = express();
+  const app = express();
 
-	app.use(json());
-	app.use(routes);
-	app.use(errorHandler);
+  app.use(
+    cors({
+      origin: process.env.FRONT_URL,
+    }),
+  );
+  app.use(json());
+  app.use(routes);
+  app.use(errorHandler);
 
-	app.listen(3333, () => console.log('🚀 App is running at port 3333'));
+  app.listen(3333, () => console.log('🚀 App is running at port 3333'));
 });
